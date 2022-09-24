ee29ab75dd3a1720859b050486515e9947ce46eb
toribettori <senngoku69@gmail.com>
2022-09-24 14:45:12 +0900

    end

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
+
+  validates :genre_id, numericality: { other_than: 1 ,message: "can't be blank"} 
+end
